532a9fd06531109d7780c9e2f0012f6b7d11ddf3
shravyarao889 <96167495+shravyarao889@users.noreply.github.com>
2022-04-26 22:41:56 +0530

    auth

---
+++ client/src/Redux/PetsReducer/reducer.js
@@ -0,0 +1,29 @@
+
+import { PETS_DATA } from "./action.js";
+
+const initState={
+    data:""
+}
+
+export const petReducer=(state=initState,{type,payload})=>{
+
+    switch (type) {
+        case PETS_DATA : 
+        return {data : payload}
+        default :
+        return state
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
